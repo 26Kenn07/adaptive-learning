8bf7042a855289bd78786a489fb475b236611268
Jatin Mamtora <91244035+Ja3mamtora@users.noreply.github.com>
2024-12-22 15:21:08 +0530

    CHG: Removed a condition from CORS in main.py

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ app.include_router(resume_checker.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
